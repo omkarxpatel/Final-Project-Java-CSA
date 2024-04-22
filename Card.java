2b7b981f589b4d92e57a0abbc38b0882c17f3d7e
CloudiCloud <qiqiwang105@gmail.com>
2024-04-22 11:12:55 -0700

    Commit

--- Card.java
+++ Card.java
@@ -5,15 +5,17 @@ public class Card {
     private int damage;
     private int cost;
     private ArrayList<String> abilities;
+    private String desc;
 
     public Card(int health, 
         int damage, 
         int cost, 
-        ArrayList<String> abilities ) {
+        ArrayList<String> abilities, String desc ) {
             this.health = health;
             this.damage = damage;
             this.cost = cost;
             this.abilities = abilities;
+            this.desc = desc;
     }
 
     public int getHealth() {return health;}
